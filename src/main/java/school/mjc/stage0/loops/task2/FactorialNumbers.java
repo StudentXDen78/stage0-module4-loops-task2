8e066fc68ff28b850109086871cbae0015058592
xden78 <venera.opt@gmail.com>
2024-10-20 22:08:26 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
-    public void printFactorialRow(int printToInclusive) {
-
+    public  void printFactorialRow(int printToInclusive) {
+        int factorialNumber = 1;
+        int i = 1;
+        while (i <= printToInclusive) {
+            factorialNumber *= i;
+            i++;
+        }
+        System.out.println(factorialNumber);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int poweredNumber = 2;
+        int i = 0;
+        while (i <= power) {
+            if (i == 0) {
+                System.out.println(1);
+                i++;
+                continue;
+            }
+            if (i == 1) {
+                System.out.println(poweredNumber);
+                i++;
+                continue;
+            }
+            poweredNumber = poweredNumber * 2;
+            System.out.println(poweredNumber);
+            i++;
+        }
     }
 }
