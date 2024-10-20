acc986c39266427e678d54a1ed60a47168344c2a
xden78 <venera.opt@gmail.com>
2024-10-20 17:55:34 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 2;
+        while (i <= printToInclusive) {
+            int a = 1;
+            int isPrime = 0;
+            while (a <= i) {
+                if (i % a == 0) {
+                    isPrime++;
+                }
+                a++;
+            }
+
+            if (isPrime == 2) System.out.println(i);
+            i++;
+        }
     }
 }
