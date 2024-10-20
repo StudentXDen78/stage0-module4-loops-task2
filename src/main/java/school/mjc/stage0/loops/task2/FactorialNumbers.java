0428ecf540f26eff0f697162425bfea2632894bd
xden78 <venera.opt@gmail.com>
2024-10-20 22:33:06 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -3,8 +3,9 @@ package school.mjc.stage0.loops.task2;
 public class FactorialNumbers {
     public  void printFactorialRow(int printToInclusive) {
         int factorialNumber = 1;
-        int i = 1;
+        int i = 0;
         while (i <= printToInclusive) {
+            if (i == 1) System.out.println(1);
             factorialNumber *= i;
             System.out.println(factorialNumber);
             i++;
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        byte sign = (byte)Math.signum(multiplyByAndToInclusive);
         int i = 0;
-        while (i <= multiplyByAndToInclusive) {
+        while (i <= (multiplyByAndToInclusive * sign)) {
             System.out.println(multiplyByAndToInclusive * i);
             i++;
         }
